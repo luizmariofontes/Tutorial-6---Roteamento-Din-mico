4c0da4b53b1e2f109c8dd798332d4cff4208791d
Luiz Mário <luizmariofontes@gmail.com>
2024-08-09 09:37:23 -0300

    Primeiro Commit

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,19 +1,24 @@
-<script setup>
-import { ref } from 'vue'
 
-defineProps({
-  event: {
-    type: Object,
-    required: true,
-  },
-})
+<script setup>
+import {ref} from 'vue'
+  defineProps({
+    event: {
+      type: Object,
+      required: true,
+    },
+  })
 </script>
 
 <template>
-  <div class="event-card">
-    <h2>{{ event.title }}</h2>
-    <span>@{{ event.time }} on {{ event.date }}</span>
-  </div>
+  <RouterLink
+    class="event-link"
+    :to="{ name: 'event-details', params: { id: event.id } }"
+  >
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </RouterLink>
 </template>
 
 <style scoped>
@@ -27,5 +32,9 @@ defineProps({
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
 }
 </style>
